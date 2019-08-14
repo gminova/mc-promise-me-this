52bd15661b43506c2afb5c440eda8f0092aa007b
gminova <minova1995@gmail.com>
2019-08-14 10:59:00 +0100

    promise

--- challenge.js
+++ challenge.js
@@ -1,35 +1,47 @@
-const https = require('https')
+const https = require("https");
 
-const makePokeUrl = pokemon => `https://pokeapi.co/api/v2/pokemon/${pokemon}/`
-const pikaUrl = makePokeUrl('pikachu')
+const makePokeUrl = pokemon => `https://pokeapi.co/api/v2/pokemon/${pokemon}/`;
+const pikaUrl = makePokeUrl("pikachu");
 
 const myApiCall = (url, callback) => {
   https
     .get(url, resp => {
-      let data = ''
-      resp.on('data', chunk => {
-        data += chunk
-      })
-      resp.on('end', () => {
+      let data = "";
+      resp.on("data", chunk => {
+        data += chunk;
+      });
+      resp.on("end", () => {
         try {
-          callback(null, JSON.parse(data))
+          callback(null, JSON.parse(data));
         } catch (e) {
-          callback('Oops, this isn\'t JSON')
+          callback("Oops, this isn't JSON");
         }
-      })
+      });
     })
-    .on('error', err => {
-      callback(err.message)
-    })
-}
+    .on("error", err => {
+      callback(err.message);
+    });
+};
 
 myApiCall(pikaUrl, (err, res) => {
-  if (err) console.log(res)
-  else console.log(res)
-})
+  if (err) console.log(res);
+  else console.log(res);
+});
 
 //Now let's make it a Promise
 
-// const myPromiseApi =
+const myPromiseApi = () => {
+  return new Promise((resolve, reject) => {
+    myApiCall(pikaUrl, (err, res) => {
+      if (err) reject(err);
+      else resolve(res);
+    });
+  });
+};
+
+//now lets call it
+myPromiseApi(pikaUrl)
+  .then((res)=> console.log(res))
+  .catch((err)=> console.log(err))
 
 //And call it here...
